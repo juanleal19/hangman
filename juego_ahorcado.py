fa1dfb2b9fb906055a9cec9041f0728bb2cf340f
Juan Leal <jsleal19@gmail.com>
2021-08-29 23:25:09 -0500

    ya medio funciona nuevo data.txt

--- juego_ahorcado.py
+++ juego_ahorcado.py
@@ -1,4 +1,17 @@
 import random
+def normalize(s):
+    replacements = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "i"),
+        ("ó", "o"),
+        ("ú", "u"),
+        ("ñ", "n")
+    )
+    for a, b in replacements:
+        s = s.replace(a, b).replace(a.upper(), b.upper())
+    return s
+
 
 def choise(): # funcion que nos ayuda a escoger una palabra de la lista 
     palabras = []
@@ -7,15 +20,17 @@ def choise(): # funcion que nos ayuda a escoger una palabra de la lista
             palabras.append(line)
 
     random_word = random.choice(palabras)
-    random_word = random_word.upper()
-    random_word = random_word[0:len(random_word)-1]
+    random_word = normalize(random_word).upper()
+    #random_word = random_word[0:len(random_word)-1]
     #print(random_word) #escogemos una palabla random de la lista
     #print(len(random_word)) #lo largo de la palabra escogida
     return random_word
 
-#def normalize(s):
-
-
+def lineas():
+    palabra = choise()
+    palabra = len(palabra)
+    palabra = palabra*"_ "
+    print(palabra)
 
 
 def run():
@@ -26,15 +41,16 @@ def run():
     ''')
     choise()
     random_word = choise()
-    print(random_word)
-    a = len(random_word)
-    b = a*"_ "
-    print(b)
-    letra = input("ponga una letra")
-    letra = str(letra.upper)
+    print(random_word) # mostrar la palaba a adivinar
+    lineas()
 
-    if letra in random_word:
-        print("adivino una letra: ")
+    while True:
+        letra = normalize(str(input("digite una letra: "))).upper()
+        print(letra)
+        if letra in random_word:
+            print("adivino una letra")
+        else:
+            print("letra incorrecta")
 
 
 
